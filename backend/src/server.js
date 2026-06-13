4a86737b6fd7c7fbd9fbfd54f9039c3eed29d663
Jamalabdiwahab <ustaadjamaal@gmail.com>
2026-06-13 14:36:44 +0300

    Add Dockerfile for multi-stage build and update server to serve static files

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,12 @@
+
 import "dotenv/config";
+
+import fs from "fs";
+import path from "path";
+
 import express from 'express';
 import cors from 'cors';
+
 import { connectToDB } from "./config/db.js";
 import { clerkMiddleware } from '@clerk/express'
 
@@ -8,6 +14,8 @@ const app=express();
 
 const PORT=process.env.PORT || 5000;
 const FRONTEND_URL=process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(), "public")
 
 app.use(express.json());
 app.use(cors({
@@ -19,6 +27,16 @@ app.use(clerkMiddleware());
 app.use("/health",(_,res)=>{
     res.json({message:"server is healthy"})
 })
+
+// if the public directory exists, serve the static files
+// this is for the production build
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 
 connectToDB().then(()=>{
     app.listen(PORT,()=>{
