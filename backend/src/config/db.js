7351d4d9af76d03ff75d4a4ff2a900d406eeeb70
Jamalabdiwahab <ustaadjamaal@gmail.com>
2026-06-13 14:54:41 +0300

    Update database connection logic to use MONGO_URI environment variable

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectToDB=async () => {
     try {
 
-        const mongoUrl=process.env.MONGO_LOCAL
+        const mongoUrl=process.env.MONGO_URI
 
         if(!mongoUrl){
             throw new Error("Mongo_url is required")
