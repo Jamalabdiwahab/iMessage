c8ba57a238f4fb88ab34360adea396321c4dd124
Jamalabdiwahab <ustaadjamaal@gmail.com>
2026-06-16 11:58:25 +0300

    feat: add axios instance and utility functions, implement authentication store with socket integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,26 @@ import AuthPage from './pages/AuthPage'
 import { ThemeProvider } from './context/ThemeContext';
 import { WallpaperProvider } from './context/WallpaperContext';
 import PageLoader from './components/PageLoader';
+import { useAuthStore } from './store/useAuthStore';
+import { useEffect } from 'react';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
 
   const { isSignedIn, isLoaded } = useAuth();
 
-  if(!isLoaded) return <PageLoader/>
+  const clearAuth = useAuthStore(state => state.clearAuth)
+  const checkAuth = useAuthStore(state => state.checkAuth)
+  const isCheckingAuth = useAuthStore(state => state.isCheckingAuth)
+
+  useEffect(()=>{
+    if(!isLoaded) return;
+
+    if(isSignedIn) checkAuth;
+    else clearAuth();
+  },[checkAuth,clearAuth,isLoaded,isSignedIn])
+
+  if(!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader/>
 
   return (
     <ThemeProvider>
@@ -20,6 +34,7 @@ function App() {
           <Route path='/' element={isSignedIn ? <ChatPage/> : <Navigate to={'/auth'} replace/>}/>
           <Route path='/auth' element={!isSignedIn ? <AuthPage/> : <Navigate to={'/'} replace/>}/>
         </Routes>
+        <Toaster/>
       </WallpaperProvider>
     </ThemeProvider>
   )
